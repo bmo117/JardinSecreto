6bb1f956df3da0525f8ea72cab0c2d9a450f265c
BMO <brandon0891996@gmail.com>
2025-07-14 05:29:02 -0600

    fix slider

--- SecreteGarden/src/componets/VerticalComparer.jsx
+++ SecreteGarden/src/componets/VerticalComparer.jsx
@@ -1,19 +1,47 @@
+// import React, { useState } from "react";
+// import ReactCompareImage from "react-compare-image";
+// import { useLockBodyScroll } from "./useLockBodyScroll";
+
+// const VerticalImageCompare = ({ before, after, altBefore, altAfter }) => {
+
+//   return (
+//     <div
+//       className="w-[90%] max-w-md mx-auto select-none touch-none rounded-lg overflow-hidden !border-2 !border-white"
+//     >
+//       <ReactCompareImage
+//         leftImage={before}
+//         rightImage={after}
+//         leftImageLabel={altBefore}
+//         rightImageLabel={altAfter}
+//         vertical={false}
+//         sliderLineColor="#fff"
+//         handleSize={40}
+//       />
+//     </div>
+//   );
+// };
+
+// export default VerticalImageCompare;
+
 import React, { useState } from "react";
 import ReactCompareImage from "react-compare-image";
-import { useLockBodyScroll } from "./useLockBodyScroll";
 
 const VerticalImageCompare = ({ before, after, altBefore, altAfter }) => {
-  // const [isDragging, setIsDragging] = useState(false);
+  const [isDragging, setIsDragging] = useState(false);
 
-  // useLockBodyScroll(isDragging);
+  React.useEffect(() => {
+    if (isDragging) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [isDragging]);
 
   return (
     <div
-      className="w-[90%] max-w-md mx-auto select-none touch-none rounded-lg overflow-hidden !border-2 !border-white"
-      // onMouseDown={() => setIsDragging(true)}
-      // onMouseUp={() => setIsDragging(false)}
-      // onTouchStart={() => setIsDragging(true)}
-      // onTouchEnd={() => setIsDragging(false)}
+      className="w-[90%] max-w-md mx-auto select-none touch-none rounded-lg overflow-hidden border-2 border-white"
+      onTouchStart={() => setIsDragging(true)}
+      onTouchEnd={() => setIsDragging(false)}
     >
       <ReactCompareImage
         leftImage={before}
@@ -23,6 +51,7 @@ const VerticalImageCompare = ({ before, after, altBefore, altAfter }) => {
         vertical={false}
         sliderLineColor="#fff"
         handleSize={40}
+        disableHover
       />
     </div>
   );
